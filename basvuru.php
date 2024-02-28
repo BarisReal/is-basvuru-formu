282a8c5e2870c7251f9e3bcbf58c10a8b61cea9f
BarisReal <133422014+Barissal@users.noreply.github.com>
2024-02-28 14:55:47 +0300

    Add files via upload

---
+++ basvuru.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+$tc = $_POST["tcKimlik"];
+$adSoyad = $_POST["adSoyad"];
+$yas = $_POST["yas"];
+$ePosta = $_POST["ePosta"];
+$profil = $_POST["profil"];
+$tarih = $_POST["date"];
+$deneyim = $_POST["deneyim"];
+$pDil = $_POST["pDil"];
+$pDil = $_POST["formOnay"];
+
+
+
+echo "TC: $tc <br>";
+echo "adSoyad: $adSoyad <br>";
+echo "yas: $yas <br>";
+echo "ePosta: $ePosta <br>";
+echo "profil: $profil <br>";
+echo "tarih: $tarih <br>";
+echo "deneyim: $deneyim <br>";
+echo "pDil: $pDil <br>";
+echo "onay: $onay <br>";
+?>
+
+</body>
+</html>
+
